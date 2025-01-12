c25de7ede4bf1554442575bc7fd761dbeb17b24c
muczc1wek <110698397+muczc1wek@users.noreply.github.com>
2025-01-12 18:47:08 +0100

    Fix installer script

--- installer/make.ps1
+++ installer/make.ps1
@@ -1,14 +1,14 @@
 echo "Downloading NSIS 3.02.1..."
 
-(New-Object Net.WebClient).DownloadFile('https://ncu.dl.sourceforge.net/project/nsis/NSIS%203/3.02.1/nsis-3.02.1-setup.exe', $env:TEMP + '\nsis-3.02.1-setup.exe')
+(New-Object Net.WebClient).DownloadFile('https://prdownloads.sourceforge.net/nsis/nsis-3.02.1-setup.exe', $env:TEMP + '\nsis-3.02.1-setup.exe')
 
 echo "Installing NSIS 3.02.1..."
 
-& $env:TEMP"/nsis-3.02.1-setup.exe" /S /D=$env:TEMP"\nsis"
+& $env:TEMP"/nsis-3.02.1-setup.exe" /S /D=$env:TEMP"/nsis"
 
 echo "Building installer of GMBT"
 
-& makensis gmbt.nsi
+& $env:TEMP"/nsis/makensis.exe" gmbt.nsi
 
 if ($LASTEXITCODE -like 0) {
 	write-host "Done without errors." -foregroundcolor "green"
